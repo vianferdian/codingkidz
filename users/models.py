07c23ab30fc4a49f51728b1bd4a3f6b693f1e755
vianferdian <vianferdian@gmail.com>
2026-09-01 21:41:04 +0700

    Set superuser role to ADMIN by default

--- users/models.py
+++ users/models.py
@@ -13,6 +13,7 @@ class CustomAccountManager(BaseUserManager):
 		other_fields.setdefault('is_staff',True)
 		other_fields.setdefault('is_superuser',True)
 		other_fields.setdefault('is_active',True)
+		other_fields.setdefault('role','ADMIN')
 
 		if other_fields.get('is_staff') is not True:
 			raise ValueError(
